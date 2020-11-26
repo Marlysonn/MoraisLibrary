54d10a9658f76969cfd54bd7ada1b9770e79b8d1
Marlysonn <marlysonnxavier@gmail.com>
2020-11-25 23:41:32 -0300

    BANCO ATUALIZADO 2

--- src/main/java/database/DataBase.java
+++ src/main/java/database/DataBase.java
@@ -46,11 +46,21 @@ public class DataBase {
     public void criaT_Acervo(){
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:banco_de_dados/DataBase.db")) {
             Statement statement = connection.createStatement();
-            statement.execute("CREATE TABLE IF NOT EXISTS ACERVO( TITULO STRING not null, EDITORA STRING not null, ANO INTEGER not null, ID INT not null, SETOR STRING not null, PRATELEIRA CHAR not null, STATUS BOOLEAN not null)");
+            statement.execute("CREATE TABLE IF NOT EXISTS ACERVO(TITULO STRING not null UNIQUE,EDITORA STRING not null,ANO INTEGER not null,ID INT not null PRIMARY KEY, ESTANTE STRING not null, PRATELEIRA STANTE not null, STATUS INTEGER not null)");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
+    
+    public void criaT_Status(){
+        try (Connection connection = DriverManager.getConnection(url)){
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS STATUS(STATUS STRING not null, ID INTEGER not null PRIMARY KEY)");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }        
+    }
+    
     /*
     public void criaT_Funcionarios(){
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:banco_de_dados/DataBase.db")) {
@@ -93,6 +103,18 @@ public class DataBase {
             pstmt.setInt(2, id);
             pstmt.executeUpdate();
         } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void insertT_Status(String status,int id){
+        String sql = "INSERT INTO STATUS(STATUS, ID) VALUES(?,?)";
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, status);
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -152,22 +174,23 @@ public class DataBase {
             System.out.println(e.getMessage());
         }
     }
-    */
-    
+    */    
     
     public static void main(String[] args) {
         DataBase app = new DataBase();
         //app.criaT_Aluno();
-        //app.criaT_Acervo();
+        app.criaT_Acervo();
         //app.criaT_Funcionarios();
         //app.criaT_Pessoa();
         //app.criaT_Funcao();
         //app.criaT_Setor();
         //app.criaT_Setores();
+        //app.criaT_Status();
         //app.updateT_Aluno(1, "Marlyson", 123123123, 9999,"ADM");
         //app.insertT_Aluno(1,"Marlyson T Xavier", "05967762418",20191022,"TI");
-        //app.insertT_Funcao("INTERCAMBIO", 6);
+        //app.insertT_Funcao("", 6);
         //app.criaT_Acervo();
-        app.insertT_Pessoa(1,"Marlyson Xavier","059.677.624-18","20191022005","TI",32072133,4);
+        //app.insertT_Pessoa(1,"Marlyson Xavier","059.677.624-18","20191022005","TI",3207,4);
+        //app.insertT_Status("ATRASO", 4);
     }
 }
